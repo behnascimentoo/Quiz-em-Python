fe8342eb92ecf821d5403f10a641b8ffcc3a7be5
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-08-06 14:58:58 -0300

    escrita da pergunta

---
+++ src/controler/quiz.py
@@ -0,0 +1,12 @@
+print("Seja muito bem vindo ao quiz da Beh!")
+answer_user = input("Quer começar? [S/N] ")
+
+if answer_user != "S":
+    quit()
+
+print("começando...")
+print("Quem desenvolveu o jogo Grand Theft Auto (GTA)? \n (A) Rockstar Games "
+      "\n (B) Ubisoft \n (C) Activision \n (D) EA \n")
+answer_1 = input("Resposta: ")
+
+if answer_1 == 
